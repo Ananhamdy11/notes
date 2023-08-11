d55b185d9f24838ff63a1cbafe8464700475b471
Anan Hamdy <nonahamdy205@gmail.com>
2023-08-12 00:52:37 +0300

    create custom app bar

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Row(
-      children: [
-       Text('Notes',style: TextStyle(
-fontSize: 28
-        ),)
-      ],
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Row(
+        children: [
+         Text('Notes',style: TextStyle(
+    fontSize: 28
+          ),),
+          Spacer(),
+          CustomSearchIcon(),
+          NoteItem()
+        ],
+      ),
+    );
+  }
+}
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(),
     );
   }
 }
